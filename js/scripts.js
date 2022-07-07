b943afac777aa011024919fb26e3d74c0698186a
francesm <fr.ncesm@gmail.com>
2022-07-07 12:57:33 -0600

    adjust main nav styles

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+/*** NAV ***/
+const mainNav = document.querySelector(".header__nav-bar");
+console.log(mainNav);
+
+/* Sticky Header */
+/* PSUEDOCODE
+- add the `position: fixed` rule to the mainNav (if js doesn't load, mainNav is unaffected)
+- add an event listener to the mainNav element that listens for a scroll event
+- when the user scrolls, check the amount scrolled on the Y axis
+    - if the user's scroll position is less than a certain amount, set the background-color to transparent
+    - if the user's scroll position is more than a certain amount, set the background-color to white
+*/
+
+mainNav.style.position = "absolute";
+mainNav.style.top = "0";
+console.log(mainNav.style);
+
+
+/* Mobile Nav */
+
+
+
+/* Search Bar */
+
+
+/*** HOME PAGE ***/
+/* Carousel */
+
+/* Search Bar */
+
+
+/*** BLOG PAGE ***/
+/* Comments Section */
+
+
+
+
+/*** CONTACT PAGE ***/
