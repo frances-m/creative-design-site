5eb3e42110c2d59fb909aad380ae57182137f783
francesm <fr.ncesm@gmail.com>
2022-07-07 17:07:00 -0600

    add slide out nav functionality and styles

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,20 @@ const addStyles = (element, style) => {
 
 
 /*** NAV ***/
-const mainNav = document.querySelectorAll("header .wrapper");
 
 /* Sticky Header */
-/* PSUEDOCODE
-- add the `position: fixed` rule to the all of elements with a class of .header__nav-bar (if js doesn't load, mainNav is unaffected)
-- if the user is one the contact page, add relative positioning to the header's display image and the main content
-    - otherwise:
-        - add an event listener to the .header__nav-bar elements that listens for a scroll event
-        - when the user scrolls, check the amount scrolled on the Y axis
-            - if the user's scroll position is less than a certain amount, set the background-color to transparent
-            - if the user's scroll position is more than a certain amount, set the background-color to white
-*/
+/* PSUEDOCODE */
+// add the `position: fixed` rule to the all of elements with a class of .header__nav-bar (if js doesn't load, mainNav is unaffected)
+// if the user is one the contact page... 
+    // add relative positioning to the header's display image and the main content
+// otherwise...
+    // add an event listener to the .header__nav-bar elements that listens for a scroll event
+    // when the user scrolls, check the amount scrolled on the Y axis
+    // if the user's scroll position is less than a certain amount, set the background-color to transparent
+    // if the user's scroll position is more than a certain amount, set the background-color to white
 
+
+const mainNav = document.querySelectorAll("header .wrapper");
 
 mainNav.forEach((navBar) => {
     // add the {position: fixed} css rule to the navBar
@@ -51,7 +52,16 @@ mainNav.forEach((navBar) => {
 
 
 /* Mobile Nav */
+/* PSUEDOCODE */
+// when a user clicks on the #slide-out-btn...
+    // toggle a class on the header that contains relevant styling
 
+const slideOutBtn = document.querySelector("#slide-out-btn");
+const header = document.querySelector("header");
+
+slideOutBtn.addEventListener('click', () => {
+    header.classList.toggle("open-slide-out");
+});
 
 
 /* Search Bar */
